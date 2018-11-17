b6e086c0e5c4fc36e14abe3905a1537657ef7cf4
romarios1987 <romios047@gmail.com>
2018-11-17 21:42:36 +0200

    Change question

--- src/components/ActiveInquirer/ActiveInquirer.js
+++ src/components/ActiveInquirer/ActiveInquirer.js
@@ -3,13 +3,13 @@ import './ActiveInquirer.css';
 import AnswersList from "./AnswersList/AnswersList";
 
 
-const ActiveInquirer = ({answers, question, onAnswerClick}) => {
+const ActiveInquirer = ({answers, question, onAnswerClick, inquirerLength, answerNumber}) => {
 
     return (
         <div className='ActiveInquirer'>
             <h3 className='Question'>
-                <span><strong>3.</strong>&nbsp;{question}</span>
-                <small>4 of 12</small>
+                <span><strong>{answerNumber}.</strong>&nbsp;{question}</span>
+                <small>{answerNumber} of {inquirerLength}</small>
             </h3>
             <AnswersList
                 answers={answers}
--- src/containers/inquirer/Inquirer.js
+++ src/containers/inquirer/Inquirer.js
@@ -5,14 +5,26 @@ import ActiveInquirer from "../../components/ActiveInquirer/ActiveInquirer";
 class Inquirer extends Component {
 
     state = {
+        activeQuestion: 0,
         inquirer: [
             {
-                question: 'Headline question?',
+                question: 'Headline question 1?',
                 rightAnswerId: 3,
+                id: 1,
                 answers: [
-                    {text: 'Answer 1', id:1},
-                    {text: 'Answer 2', id:2},
-                    {text: 'Answer 3', id:3}
+                    {text: 'Answer 1', id: 1},
+                    {text: 'Answer 2', id: 2},
+                    {text: 'Answer 3', id: 3}
+                ]
+            },
+            {
+                question: 'Headline question 2?',
+                rightAnswerId: 1,
+                id: 2,
+                answers: [
+                    {text: 'Answer 1', id: 1},
+                    {text: 'Answer 2', id: 2},
+                    {text: 'Answer 3', id: 3}
                 ]
             }
         ]
@@ -21,6 +33,10 @@ class Inquirer extends Component {
 
     onAnswerClickHandler = (answerId) => {
         console.log(`answerId: ${answerId}`);
+
+        this.setState({
+            activeQuestion: this.state.activeQuestion + 1
+        })
     };
 
 
@@ -29,9 +45,11 @@ class Inquirer extends Component {
             <div className='Inquirer'>
                 <h1>Inquirer</h1>
                 <ActiveInquirer
-                    answers={this.state.inquirer[0].answers}
-                    question={this.state.inquirer[0].question}
+                    answers={this.state.inquirer[this.state.activeQuestion].answers}
+                    question={this.state.inquirer[this.state.activeQuestion].question}
                     onAnswerClick={this.onAnswerClickHandler}
+                    inquirerLength={this.state.inquirer.length}
+                    answerNumber={this.state.activeQuestion + 1}
                 />
             </div>
         )
