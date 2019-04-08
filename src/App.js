70152a0591227d9d29ddd8fd429c9332148d255f
romarios1987 <romios047@gmail.com>
2019-04-08 20:07:54 +0300

    Initial Project (second variant)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React, {Component} from 'react';
-import Layout from "./hoc/Layout/Layout";
-import {Route, Switch} from 'react-router-dom';
-import Inquirer from "./containers/Inquirer/Inquirer";
-import Auth from "./containers/Auth/Auth";
-import InquirerCreator from "./containers/InquirerCreator/InquirerCreator";
-import InquirerList from "./containers/InquirerList/InquirerList";
+import './App.css';
 
 class App extends Component {
-    render() {
-        return (
-            <Layout>
-                <Switch>
-                    <Route path={'/auth'} component={Auth}/>
-                    <Route path={'/inquirer-creator'} component={InquirerCreator}/>
-                    <Route path={'/inquirer/:id'} component={Inquirer}/>
-                    <Route path={'/'} component={InquirerList}/>
-                </Switch>
-            </Layout>
-        );
-    }
+  render() {
+    return (
+          <div className="App">
+            <h2>Hello React!</h2>
+          </div>
+    );
+  }
 }
 
 export default App;
